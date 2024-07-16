b54152d8caa66f0754319bf1b030b25edb917286
Steven Reyes <steven.reyes88@gmail.com>
2024-07-16 20:37:16 +0900

    collab new and create initialized

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,2 +1,19 @@
 class CollaborationsController < ApplicationController
+  def new
+    @collaboration = Collaboration.new
+  end
+
+  def create
+    @collaboration = Collaboration.new(collaboration_params)
+    @brand = Brand.find(params[:brand_id])
+    if @collaboration.save
+      redirect_to collaboration_path(@collaboration)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def collaboration_params
+    params.require(:collaboration).permit(:title, :description, :price, :category, :start_date, :end_date)
+  end
 end
