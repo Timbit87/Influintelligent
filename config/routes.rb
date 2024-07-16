b8d521f3f84f21c5150691773ff183f7cf3a66a0
Andrew Beckett <andrewbeckett95@gmail.com>
2024-07-16 21:02:13 +0900

    Routes done. Maybe

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :collaborations, only: [:index, :new, :create, :show] do
+    resources :submissions, only: [:create, :new]
+  end
+
+  resources :submissions, only: [:index, :edit, :update, :destroy]
+
+  namespace :brand do
+    resources :collaborations do
+      resources :submissions, only: :index
+    end
+  end
 end
