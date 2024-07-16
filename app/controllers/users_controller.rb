ccfc4485843592c6aa3502eda9b8fe8ecda19599
Chika Sakai <c.sakai1101@gmail.com>
2024-07-16 20:33:58 +0900

    edited user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_influencer, only: %i[index, new, show, edit]
+  # this is influencers controller
+
+  def index
+    @influencers = User.where(brand: true)
+  end
+
+  def new
+  end
+
+  def create
+    @influencer = User.new
+    @influencer.save
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_influencer
+    @influencer = User.find(params[:id])
+  end
+
 end
