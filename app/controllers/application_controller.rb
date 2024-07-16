565f74d86ec201240ef75163fd2de72f13bdec32
narumi katayama <narumikataiama@icloud.com>
2024-07-16 20:34:22 +0900

    sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def after_sign_up_path_for(resource)
+    if resource.brand?
+      brand_path(resource)
+      redirect_to brand_path(resource)
+    else
+      influencer_path(resource)
+    end
+  end
 end
