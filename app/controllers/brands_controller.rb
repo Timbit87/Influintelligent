1efeb31762ae9784c4350babb7d460232d19100d
Steven Reyes <steven.reyes88@gmail.com>
2024-07-16 20:03:41 +0900

    new and create methods defined in Brands controller. Brand Params defined

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,18 @@
 class BrandsController < ApplicationController
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    if @brand.save
+      redirect_to brand_path(@brand)
+    else
+      render 'new'
+    end
+  end
+
+  def brand_params
+    params.require(:brand).permit(:name, :description)
+  end
 end
