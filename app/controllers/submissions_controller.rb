4c875a5e640b64f8d5758ef72e2b8ea26ac42d6d
Chika Sakai <c.sakai1101@gmail.com>
2024-07-17 21:15:46 +0900

    updated submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,16 @@
 class SubmissionsController < ApplicationController
+
+  #As an influencer, I can view my current collaborations
+
+  def index
+    @collaborations = Collaboration.all
+  end
+
+  def update
+  end
 end
+
+
+resources :submissions, only: [:index, :update]
+  # the accept/reject submission buttons are on the submissions index page
+  # :destroy is not first priority
