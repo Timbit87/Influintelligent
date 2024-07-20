6d642efdf3b6880effaa91d28caf3c3c574e59ea
Chika Sakai <c.sakai1101@gmail.com>
2024-07-20 11:46:11 +0900

    Update model with dependency to run seeds without error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :collaborations, dependent: :destroy
+  has_many :submissions, dependent: :destroy
+
 
   has_one_attached :avatar
 
