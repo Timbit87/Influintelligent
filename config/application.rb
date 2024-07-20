c41fa4095bd97d2120c0f7c9725d8340aaa888fa
Andrew Beckett <andrewbeckett95@gmail.com>
2024-07-20 15:50:19 +0900

    fixed heroku assests. maybe

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Influintelligent
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.serve_static_assets = true
   end
 end
