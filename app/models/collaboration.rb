addfd36caca8b557edc6cfa14538b360cbb40c44
narumi katayama <narumikataiama@icloud.com>
2024-07-24 11:25:47 +0900

    collaboration new form

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -2,11 +2,13 @@ class Collaboration < ApplicationRecord
   CATEGORIES = ['Fashion', 'Beauty', 'Lifestyle', 'Travel', 'Food', 'Fitness', 'Tech', 'Art', 'Music', 'Sports', 'Toy', 'Game', 'Electronic', 'Stationary'].freeze
 
   validates :title, :description, :category, :price, :start_date, :end_date, presence: true
-  validates :category, inclusion: { in: CATEGORIES.map(&:downcase) }, allow_nil: false
+  validates :category, inclusion: { in: CATEGORIES }, allow_nil: false
   belongs_to :user
   has_many :submissions, dependent: :destroy
   before_save :capitalize_title, :capitalize_categories
   validate :user_brand
+
+  has_one_attached :banner 
 
   private
 
