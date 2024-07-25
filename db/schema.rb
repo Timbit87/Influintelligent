a1a8b0e1ef0fe7ec3b9cc72dc16a4574a81df223
Steven Reyes <steven.reyes88@gmail.com>
2024-07-25 20:42:15 +0900

    changed websites to website. Changed website from array to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_104034) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_113805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,13 +78,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_104034) do
     t.string "brand_name"
     t.string "address"
     t.string "contact"
-    t.string "websites", array: true
     t.json "social_links"
     t.date "availability"
     t.text "about"
     t.string "tags", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "website"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
