e30ea0f3d2cb3723a0869766c38b29c2b46bac96
Chika Sakai <c.sakai1101@gmail.com>
2024-07-25 19:57:57 +0900

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_20_081748) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_104034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_20_081748) do
     t.datetime "updated_at", null: false
     t.bigint "collaboration_id", null: false
     t.integer "status", default: 0
+    t.text "message"
     t.index ["collaboration_id"], name: "index_submissions_on_collaboration_id"
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
