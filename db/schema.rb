a796870d9bf2b0f803c30e80290caede9127b8c4
narumi katayama <narumikataiama@icloud.com>
2024-09-03 13:09:47 +0900

    fix photo and scrapping

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_27_035924) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_113805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,13 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_035924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_collaborations_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
-    t.text "content"
   end
 
   create_table "submissions", force: :cascade do |t|
